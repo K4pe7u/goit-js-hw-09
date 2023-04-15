339b578abd5d78a00110acde1ef30ed1cb2c028d
Juliusz Janicki <juliusz.janicki@gmail.com>
2023-04-15 13:17:04 +0200

    Part 4
    ex 3 + fixed 1,2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,67 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+const delayInput = form.querySelector('input[name="delay"]');
+const stepInput = form.querySelector('input[name="step"]');
+const amountInput = form.querySelector('input[name="amount"]');
+
+// const submitBtn = form.querySelector('button[type="submit"]'); -> zbędne
+
+// position -> numer promisa
+// delay -> opóźnienie wykonania promisa
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  // wywołanie
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      // generowanie losowego numeru -> jesli większy od 0.3 w zmiennej shouldResolve = resolve(wykonane)
+      // jesl nie jest większy = reject
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+// zdarzenie buttona, na formie z sumbitem bez query
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  // numbery do wartości wpisanych w inputy
+  let firstDelay = +delayInput.value;
+  let delayStep = +stepInput.value;
+  let amount = +amountInput.value;
+
+  // iteracja amount do wykonania kolejnej funkcji (sprawdzić czy mozna zapisać inaczej )
+  for (let i = 0; i < amount; i++) {
+    // ok iteracje ujmuje w do działania (suma inputów razy iterowana pozycja (chyba))
+    const promiseDelay = firstDelay + i * delayStep;
+    createPromise(i + 1, promiseDelay)
+      //    if (shouldResolve) {
+      // resolve({ position, delay });
+      // } else {
+      // reject({ position, delay });
+      // }
+      // then + catch
+
+      // wywołanie tekstowe
+      // resolve
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      // reject
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
+
+// sorry za ilość komentarzy
