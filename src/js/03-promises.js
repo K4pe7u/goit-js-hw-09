8b29468dbe9015dff08dfd6c910f8bcb30217e92
Juliusz Janicki <117281053+K4pe7u@users.noreply.github.com>
2023-04-18 10:46:15 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -65,3 +65,42 @@ form.addEventListener('submit', event => {
 });
 
 // sorry za ilość komentarzy
+
+
+// Zajęcia przykład:
+// const formEl = document.querySelector(".form");
+
+// formEl.addEventListener("submit", (ev) => {
+//   ev.preventDefault();
+  
+//   const formData = new FormData(ev.target);
+//   const firstDelay = +formData.get("delay");
+//   const step = +formData.get("step");
+//   const amount = +formData.get("amount");
+  
+//   for(let i = 0; i < amount; i++) {
+//     createPromise(i + 1, firstDelay + i * step)  
+//       .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+//   }
+ 
+// })
+
+// function createPromise(position, delay) {
+//   return new Promise ((resolve, reject) => {
+//     setTimeout(() => {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+// resolve({position, delay});
+//   } else {
+// reject({position, delay});
+//   }
+//                      }, delay);
+//   });
+// }
+
+
